334d7c8c731c75baa2dab018ab3660d1d9796648
DataNaija <139554441+DataNaija@users.noreply.github.com>
2023-07-31 02:46:16 -0600

    Codes

--- dy_1.py
+++ dy_1.py
@@ -1,5 +1,4 @@
 print("Welcome to Band Name Generator App")
-
 city = input("Enter the name of the city where you grew up:\n ")
 pet = input("Enter your favorite pet name:\n ")
 print( "Your band name is " + city + " " + pet)
